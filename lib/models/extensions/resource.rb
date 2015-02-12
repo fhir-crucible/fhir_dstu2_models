c05ef2463a02f628fd5925a9a77d16f37d588b05
Andre Quina <andre.quina@gmail.com>
2015-02-12 17:21:04 -0500

    added method to be able to load a resource from a string (either xml or json)

--- lib/models/extensions/resource.rb
+++ lib/models/extensions/resource.rb
@@ -12,6 +12,17 @@ module FHIR
       base.send(:field, :language, type: String)
     end
 
+    def self.from_contents(contents)
+      doc = Nokogiri::XML(contents)
+      if doc.errors.empty?
+        doc.root.add_namespace_definition('fhir', 'http://hl7.org/fhir')
+        doc.root.add_namespace_definition('xhtml', 'http://www.w3.org/1999/xhtml')
+        "FHIR::#{doc.at_xpath('/*').name}".constantize.from_xml(contents)
+      else
+        "FHIR::#{JSON.parse(contents)['resourceType']}".constantize.from_fhir_json(contents)
+      end
+    end
+
     class ResourceMetaComponent
       include Mongoid::Document
       include FHIR::Element
