baaccde2c4c67fe0d7ace4a138dfe2305b45d769
Reece Adamson <radamson@mitre.org>
2019-02-07 15:30:36 -0500

    small changes for rubocop

--- lib/fhir_dstu2_models/fhir_ext/structure_definition.rb
+++ lib/fhir_dstu2_models/fhir_ext/structure_definition.rb
@@ -225,9 +225,9 @@ module FHIR
                 unless has_valid_code
                   vcc.coding.each do |c|
                     check_fn = self.class.vs_validators[c.system]
-                    if check_fn
+                    if check_fn && check_fn.call(c)
                       binding_issues << "#{describe_element(element)} has no codings from it's specified system: #{c.system}.  "\
-                                        "Codings evaluated: #{vcc.to_json}" unless check_fn.call(c)
+                                        "Codings evaluated: #{vcc.to_json}"
                     end
                   end
                 end
